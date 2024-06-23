2d8dd70412ac23fbda6ee717a83a2a5d5e88264d
teamchartvn <146596976+teamchartvn@users.noreply.github.com>
2024-06-23 22:53:16 +0700

    Update signal.cs

--- signal.cs
+++ signal.cs
@@ -1,7 +1,3 @@
-//
-// Copyright (C) 2022, NinjaTrader LLC <www.ninjatrader.com>.
-// NinjaTrader reserves the right to modify or overwrite this NinjaScript component with each release.
-//
 #region Using declarations
 using System;
 using System.Collections.Generic;
@@ -18,261 +14,193 @@ using NinjaTrader.Cbi;
 using NinjaTrader.Gui;
 using NinjaTrader.Gui.Chart;
 using NinjaTrader.Gui.SuperDom;
+using NinjaTrader.Gui.Tools;
 using NinjaTrader.Data;
 using NinjaTrader.NinjaScript;
 using NinjaTrader.Core.FloatingPoint;
 using NinjaTrader.NinjaScript.DrawingTools;
 #endregion
 
-// This namespace holds indicators in this folder and is required. Do not change it.
-#region obv
-
+//This namespace holds Indicators in this folder and is required. Do not change it. 
 namespace NinjaTrader.NinjaScript.Indicators
 {
+	public class  DinhDay{
+		
+		public int bar;
+		public bool diemCao;
+		public double giaDinhDay;
+		
+		public DinhDay(int bar_index , bool dinhorday, double gia)
+		{
+			this.bar = bar_index;
+			this.diemCao = dinhorday;
+			this.giaDinhDay = gia;
+		}
+	}
 	
-	
-	
-    public class OBVWithColor : Indicator
+	public class ZZ : Indicator
     {
-        private double previousObv;
-        private Series<double> obv;
-        
-		private double lastDelta;
-		private List<double> deltaList;
-		private Series<int> tnrSignal;
-		
-		// Series chứa nến có tín hiệu vào
-		
-		private Series<int> Lenh;  //  -1 bán -2 bán kiểu 2 , 0 null , 1 mua 1 , 2 mua kiểu 2  . Các lệnh đã được bbaams
-        
-		private Series<int> SigleNote;    // lưu các vị trí có tín hiệu dù ko vào lệnh , để tín hiệu sau check xem khoảng cách gần nhất có tín hiệu ngược chiều ko
-		
+        private double lastHigh;
+        private double lastLow;
+        private int lastHighBar;
+        private int lastLowBar;
+        private int trendDir;
+		private Series<double>  ZigZag;
+        private bool timDinh =true; // true tăng , false giảm
+        private double Deviation;
+        private int Backstep;
+		private int Depth;
+		private List<DinhDay> drawdinhday;  // chứa dữ liệu các đỉnh đáy để vẽ
+		//private bool timDinh;
+		//private double lastHighDinh , lastLowDay;
         protected override void OnStateChange()
         {
             if (State == State.SetDefaults)
             {
-                Description = @"Tính toán obv và delta Tunaround.";
-                Name = "1.obvmod";
+                Description = @"ZigZag indicator with custom depth, deviation, and backstep.";
+                Name = "1.zz";
                 Calculate = Calculate.OnEachTick;
-                IsOverlay = false;
-
-                // Thêm một plot với màu xanh mặc định
-                AddPlot(Brushes.Blue, "OBVPlot");
-            }
-            else if (State == State.DataLoaded)
-            {
-                obv = new Series<double>(this);
+                IsOverlay = true;
+                //AddPlot(Brushes.Goldenrod, "ZigZag");
+				
+				
+				// Thiết lập các giá trị mặc định cho ZZ
+				//Deviation =0.05;
+				//Backstep =3;
+				//Depth =12;
+				SonenDebug =30;
+				
             }
 			else if (State == State.Configure)
 			{
-				lastDelta = 0.0;
-				tnrSignal = new Series<int>(this);
-				deltaList = new List<double>();
-				Lenh = new Series<int>(this);
-				SigleNote = new Series<int>(this);
-				
+			
+				 lastLow =lastLowBar =0; // Khi bắt đầu tất cả = 0
+				 lastHigh = lastHighBar =0;
+				 Deviation =10*TickSize;
+				 Backstep =3;
+				 Depth =12;
+				 timDinh =true;
+				drawdinhday = new List<DinhDay>();
+			}
+			else if (State == State.DataLoaded)
+			{
+					ZigZag = new Series<double>(this);
+				   // drawdinhday = new Series<DinhDay>(this);
 			}
         }
 
         protected override void OnBarUpdate()
         {
-            if (CurrentBar == 0)
-            {
-                obv[0] = Volume[0];
-                previousObv = obv[0];
-                return;
-            }
-            
-            // Tính toán OBV
-            if (Close[0] > Close[1])
-			{ obv[0] = obv[1] + Volume[0];}
-            else if (Close[0] < Close[1])
-			{ obv[0] = obv[1] - Volume[0];}
-            else{
-				  obv[0] = obv[1];
-			}
-              
-             
-			if(IsFirstTickOfBar && CurrentBars[0] > 0)
-            {
-				deltaList.Add(lastDelta);
-					lastDelta = 0.0;
-				if(CurrentBars[0] >=2 )
-					{
-					      KeoOBV();
-					}
+			if( BarsInProgress ==0)
+			{
+			
+			if(!IsFirstTickOfBar || CurrentBars[0] <2 )
+			{
+				if( CurrentBars[0] ==1)
+				{
+				 //  Print("??? " + High[CurrentBars[0]-1]);
+				  lastHigh =  BarsArray[0].GetHigh(0);
+				  lastLow   = BarsArray[0].GetLow(0);
+				}
 				
+				return;
 			}
-//            // Thiết lập màu sắc cho plot
-//            if (obv[0] > previousObv)
-//                PlotBrushes[0][0] = Brushes.Green;
-//            else if (obv[0] < previousObv)
-//                PlotBrushes[0][0] = Brushes.Red;
-			// So sánh màu khi obv cũ trước đó
-			PlotBrushes[0][0] = obv[0] >= previousObv ? Brushes.Green : Brushes.Red;
-            
-            // Cập nhật giá trị OBV cho plot
-            Values[0][0] = obv[0];
 			
+			// DEBUG Chi chay toi nen nay : thiet lap so nen muon test ma
 			
+			if(CurrentBar > SonenDebug)
+				return;
 			
+			// END DEBUG
 			
-			// cuối cùng mới cập nhật obv mới
-            previousObv = obv[0];
-        }
-		
-		
-		
-		private void KeoOBV(){
-				if (KtraDojBua(1) ==true || Volume[1] < Volume[2]) // Nếu là nến doj nến búa = done
-							return;
+			Draw.Text(this, (CurrentBars[0]-1).ToString()+ "index",(CurrentBars[0]-1).ToString(),1 , High[1] + 5*TickSize);
+			Print(CurrentBar + " (H) => " + lastHigh.ToString() + " ih : " + lastHighBar.ToString() + "    ( L) =>" + lastLow.ToString() + " il : " + lastLowBar.ToString() + timDinh.ToString() );
+				if(timDinh){
+					
+					if(High[1] >=lastHigh){
+						lastHigh = High[1];
+						lastHighBar = CurrentBar-1;
+						//vẽ
 						
-						var sigleDelta = Turnaround();
-						if(sigleDelta ==1){
-							SigleNote[1] =3;
-//							if (ThanNen())
-//								return;
-//							// kiếm tra nến liền trước [2] có lệnh ngược chiều sell  ko và volume ra sao ?
-//							if (CheckLenhTruoc(3,true) & Volume[1] < Volume[2])
-//							{
-//								Draw.Text(this,"huy"+ CurrentBar,"hmua",1,BarsArray[0].GetLow(CurrentBars[0]-1) + 8* TickSize);
-//								return;
-//							} else
-//							{
-//								if (obv[1] > obv[2])
-//								{
-//								       Draw.ArrowUp(this, "mua1"+CurrentBar, true, 1, BarsArray[0].GetLow(CurrentBars[0]-1) - 8* TickSize,Brushes.Blue,true);
-//								      SigleNote[1] =1;
-//							     }
-//							    else if (obv[1] > obv[2] & obv[1] > obv[3]){
-//							          	Draw.ArrowUp(this, "mua2"+CurrentBar, true, 1, BarsArray[0].GetLow(CurrentBars[0]-1) - 8* TickSize,Brushes.Cyan,true);
-//							          	SigleNote[1] =2;
-//							          }
-//							}
-							
+					}
+					else{  // Nếu có đỉnh mà trend bắt đầu giảm
+						
+						if(CurrentBar-1 -lastHighBar >= Backstep){
+							Print(" Xac nhan DINH Backstep " +lastHighBar);
+							 // in giá trị HH ( đỉnh của zz )
+							//ZigZag[0] = lastHigh;
+							//var Dinh = new DinhDay(lastHighBar,true,lastHigh);
+							Print(CurrentBar + " (H1) => " + lastHigh.ToString() + " ih : " + lastHighBar.ToString() + "    (L1) =>" + lastLow.ToString() + " il : " + lastLowBar.ToString() + timDinh.ToString() );
+							Draw.Dot(this, "dinh" + lastHighBar, true, CurrentBar - lastHighBar, lastHigh + 5*TickSize, Brushes.Cyan);// cos nhaam lan ve lasthigbar
+							//Draw.Text(this, (CurrentBars[0]-1).ToString()+ "đỉnh",(CurrentBars[0]-1).ToString(,1 , High[1] + 5*TickSize);
+							//drawdinhday.Add (new DinhDay(lastHighBar,true,lastHigh));
+							if(CurrentBar-1 - lastHighBar >= Depth && lastHigh - Low[1] >= Deviation)
+							{
+								 
+								lastLow = Low[1];  // Đặt lại giá trị lastLow
+								timDinh = false; // đã xác định được đáy thỏa mãn 3 điều kiện
+							}
+						}
+					}
+				}
+				else   // timDInh =false ( tạo đáy )
+				{
+					if(Low[1] <= lastLow){
+						lastLow = Low[1];           // giá thấp hơn tiếp tục tạo đáy thấp
+					    lastLowBar = CurrentBar-1;
+						
+					}
+					
+					else{// nếu có đáy mà giá tăng trend up khi có đáy
+						if(CurrentBar-1 - lastLowBar >= Backstep)  // hiển thị đáy
+						{
+							Print(" Xac nhan DAY Backstep");
+							//ZigZag[1] = lastLow;
+							//drawdinhday[0] =  new DinhDay(lastLowBar,false,lastLow);
+							 Draw.Dot(this, "day"+lastLowBar.ToString(), true,CurrentBar- lastLowBar,lastLow - 5*TickSize, Brushes.Violet);
+							//drawdinhday.Add ( new DinhDay(lastLowBar,false,lastLow));
+							if(CurrentBar-1 - lastLowBar  >= Depth && High[1] - lastLow >= Deviation)
+							{
 								
+								lastHigh = High[1];
+								timDinh = true;
+							}
 						}
-						else if (sigleDelta == -1){
-							SigleNote[1] =-3;
-							// kiếm tra nến liền trước [2] có lệnh ngược chiều ( (buy )ko và volume ra sao ?
-//							if (CheckLenhTruoc(3,false) & Volume[1] < Volume[2])
-//							{
-//								Draw.Text(this,"huy"+ CurrentBar,"hban",1,BarsArray[0].GetHigh(CurrentBars[0]-1) + 8* TickSize);
-//								return;
-//							} else
-//							{
-//								if (obv[1] < obv[2]){
-//								     Draw.ArrowDown(this, "ban1"+CurrentBar, true, 1, BarsArray[0].GetHigh(CurrentBars[0]-1) +8* TickSize,Brushes.OrangeRed,true);
-//								     SigleNote[1] =-1;
-//							     }
-//							     else if (obv[1] < obv[2] & obv[1] < obv[3]){
-//							     	Draw.ArrowDown(this, "ban2"+CurrentBar, true, 1, BarsArray[0].GetHigh(CurrentBars[0]-1) + 8* TickSize,Brushes.Red,true);
-//							     	SigleNote[1] =-2;
-//							        }
-//							}
-							
-						}
-		}
-		private bool CheckLenhTruoc(int range,bool xanh){  // số nến tối đa mà xuất hiện lệnh ngược chiều => done : range có thể cấu hình biến nhập từ GUI
-			for(int i = 1; i< 1+ range; i++)
-			{
-				if(xanh==true & SigleNote[i] <0)
-					return true; // done
-				if(xanh ==false & SigleNote[i] >0)
-					return true;  // done
-			}
-			return false;
-		}
-		
-		public bool ThanNen(){
-			if(Math.Abs(Open[1]-Close[1]) < 0.5* Math.Abs(Open[2]-Close[2]))
-				return true;
-			
-			return false;
-		}
-		public bool KtraDojBua(int i)
-		{
-			if( Math.Abs(Open[i]-Close[i])< 0.35*(High[i]-Low[i]))  // nếu thân nến < 25% chiều dài nến
-				return true;
-			
-			return  false;
-		}
-		// Delta TRN
-		private int Turnaround()
-		{
-			//  Chọn kiểu tính toán sma volum
-		
-			// xanh 
-			if(deltaList[CurrentBars[0]-1] > 0)
-			{
-                if(deltaList[CurrentBars[0]-2] > 0)
-				  return 0; 
-				if(BarsArray[0].GetLow(CurrentBars[0]-1) <= BarsArray[0].GetLow(CurrentBars[0]-2) && BarsArray[0].GetClose(CurrentBars[0]-1) > BarsArray[0].GetOpen(CurrentBars[0]-1))
-				{
-					// Tín hiệu delta TNR up
-					tnrSignal[1] = 1;
-					//Draw.ArrowUp(this, "mua"+CurrentBar, true, 1, BarsArray[0].GetLow(CurrentBars[0]-1) - 12* TickSize,Brushes.Green,true);
-					Draw.Dot(this, "mua"+CurrentBar,  true,  1, BarsArray[0].GetLow(CurrentBars[0]-1) - 12* TickSize,Brushes.Cyan);
-					//Draw.ArrowUp(this, "mua"+CurrentBar, true, 1, BarsArray[0].GetLow(CurrentBars[0]-1) - 8* TickSize,volup_color,true);
-					// Thêm vào mảng của delta phân kỳ 
-					//BoT.delta_phanky.Add(CurrentBars[0]-1);
+					}
 					
-					return 1; // Delta Tunaround ok => done
 				}
-					
-			}else 
-			{ // đỏ
-				if(deltaList[CurrentBars[0]-2] < 0)
-					return 0;
-				if(BarsArray[0].GetHigh(CurrentBars[0]-1) >= BarsArray[0].GetHigh(CurrentBars[0]-2) &&BarsArray[0].GetClose(CurrentBars[0]-1) < BarsArray[0].GetOpen(CurrentBars[0]-1))
-				{
-					// Tín hiệu delta TNR down
-					//Draw.ArrowDown(this, "ban"+CurrentBar, true, 1, BarsArray[0].GetHigh(CurrentBars[0]-1) + 12* TickSize,Brushes.Yellow,true);
-					tnrSignal[1] = -1;
-						Draw.Dot(this, "bán"+CurrentBar,  true,  1, BarsArray[0].GetHigh(CurrentBars[0]-1) + 12* TickSize,Brushes.Violet);
-					//Draw.ArrowDown(this, "ban"+CurrentBar, true, 1,BarsArray[0].GetHigh(CurrentBars[0]-1) + 2* TickSize,voldow_color,true);
-					// BoT.delta_phanky.Add(CurrentBars[0]-1);
-					return -1; // delta tunaround ok done !
-				}
-			}
-			tnrSignal[1] = 0;
-			return 0;
-		}
-		// Hàm tính Delta
-		protected override void OnMarketData(MarketDataEventArgs e)
-		{			
-			if(e.MarketDataType == MarketDataType.Last)
-			{				
-				if(e.Price >= e.Ask)
-				{
-					//buyTmp += e.Volume;
-					lastDelta  += e.Volume;
-				}
-				else if (e.Price <= e.Bid)
-				{
-				//	sellTmp += e.Volume;
-					lastDelta -= e.Volume;
-				}
-			}	
-		}
-		
-		
-		
-		private double[] GetPriceI2(int i)
-		{
-		     double[] rT = new double[4];
-			rT[0]= BarsArray[0].GetOpen(CurrentBars[i]);
-			rT[1]= BarsArray[0].GetClose(CurrentBars[i]);
-			rT[2]= BarsArray[0].GetHigh(CurrentBars[i]);
-			rT[3]= BarsArray[0].GetLow(CurrentBars[i]);
-			return rT;
-		}
 				
 				
-    }
-}
+				
+				
+			
+			}//end CurrentBars
+	
+			
+		}//end func
+		
+		
+		private void DrawZigZag(){
+			 for (int i = 0 ; i < drawdinhday.Count-1 ;i ++){
+			 	//Draw.Line(NinjaScriptBase owner, string tag, bool isAutoScale, int startBarsAgo, double startY, int endBarsAgo, double endY, Brush brush, DashStyleHelper dashStyle, int width, bool drawOnPricePanel)
+				// Draw.Line(this, "ZZ" + i.ToString(),true,drawdinhday[i].bar,drawdinhday[i].giaDinhDay,drawdinhday[i+1].bar,drawdinhday[i+1].giaDinhDay,Brushes.Yellow,DashStyleHelper.Dot,2,true);
+				 Draw.Dot(this, i.ToString(), true, i, drawdinhday[i].giaDinhDay, Brushes.Violet);
+			 }
+		}
+		
+		  
+		 #region Properties
+		[NinjaScriptProperty]
+		[Range(12, int.MaxValue)]
+		[Display(Name="Sonen", Description="sonedebug", Order=1, GroupName="Parameters")]
+		public int SonenDebug{ get; set; }
+		#endregion
+            
+        } // end class
 
-#endregion
+    
+    
+}
 
 #region NinjaScript generated code. Neither change nor remove.
 
@@ -280,19 +208,19 @@ namespace NinjaTrader.NinjaScript.Indicators
 {
 	public partial class Indicator : NinjaTrader.Gui.NinjaScript.IndicatorRenderBase
 	{
-		private OBVWithColor[] cacheOBVWithColor;
-		public OBVWithColor OBVWithColor()
+		private ZZ[] cacheZZ;
+		public ZZ ZZ(int sonenDebug)
 		{
-			return OBVWithColor(Input);
+			return ZZ(Input, sonenDebug);
 		}
 
-		public OBVWithColor OBVWithColor(ISeries<double> input)
+		public ZZ ZZ(ISeries<double> input, int sonenDebug)
 		{
-			if (cacheOBVWithColor != null)
-				for (int idx = 0; idx < cacheOBVWithColor.Length; idx++)
-					if (cacheOBVWithColor[idx] != null &&  cacheOBVWithColor[idx].EqualsInput(input))
-						return cacheOBVWithColor[idx];
-			return CacheIndicator<OBVWithColor>(new OBVWithColor(), input, ref cacheOBVWithColor);
+			if (cacheZZ != null)
+				for (int idx = 0; idx < cacheZZ.Length; idx++)
+					if (cacheZZ[idx] != null && cacheZZ[idx].SonenDebug == sonenDebug && cacheZZ[idx].EqualsInput(input))
+						return cacheZZ[idx];
+			return CacheIndicator<ZZ>(new ZZ(){ SonenDebug = sonenDebug }, input, ref cacheZZ);
 		}
 	}
 }
@@ -301,14 +229,14 @@ namespace NinjaTrader.NinjaScript.MarketAnalyzerColumns
 {
 	public partial class MarketAnalyzerColumn : MarketAnalyzerColumnBase
 	{
-		public Indicators.OBVWithColor OBVWithColor()
+		public Indicators.ZZ ZZ(int sonenDebug)
 		{
-			return indicator.OBVWithColor(Input);
+			return indicator.ZZ(Input, sonenDebug);
 		}
 
-		public Indicators.OBVWithColor OBVWithColor(ISeries<double> input )
+		public Indicators.ZZ ZZ(ISeries<double> input , int sonenDebug)
 		{
-			return indicator.OBVWithColor(input);
+			return indicator.ZZ(input, sonenDebug);
 		}
 	}
 }
@@ -317,14 +245,14 @@ namespace NinjaTrader.NinjaScript.Strategies
 {
 	public partial class Strategy : NinjaTrader.Gui.NinjaScript.StrategyRenderBase
 	{
-		public Indicators.OBVWithColor OBVWithColor()
+		public Indicators.ZZ ZZ(int sonenDebug)
 		{
-			return indicator.OBVWithColor(Input);
+			return indicator.ZZ(Input, sonenDebug);
 		}
 
-		public Indicators.OBVWithColor OBVWithColor(ISeries<double> input )
+		public Indicators.ZZ ZZ(ISeries<double> input , int sonenDebug)
 		{
-			return indicator.OBVWithColor(input);
+			return indicator.ZZ(input, sonenDebug);
 		}
 	}
 }
